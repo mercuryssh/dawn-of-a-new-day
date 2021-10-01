004786f419088bdb22d90f878c9157d7994f5f06
Joseph Estevez <mateobackground@gmail.com>
2021-10-01 11:40:46 -0400

    past linter

--- src/services/time.ts
+++ src/services/time.ts
@@ -1,30 +1,30 @@
-import { TimeSet, DaySet } from './definitions'
+import { TimeSet } from './definitions'
 
 export default class Time {
   private date: Date
   private day: number
 
-  constructor() {
+  constructor () {
     this.date = new Date()
     this.day = this.date.getDate()
   }
 
-  getSet(): string {
+  getSet (): string {
     if (this.date.getHours() < 8) return TimeSet.DAWN
     else if (this.date.getHours() < 20) return TimeSet.NOON
     else return TimeSet.SunSet
   }
 
-  getDay(): string {
-    let greeter
+  getDay (): string {
+    let greeter:string
     this.day === 15 || this.day === 30 ? greeter = 'The Final' : greeter = 'A New'
     return greeter
   }
 
-  getRemainTime(): number {
-    let fullHours: number = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0).getDate() * 24;
-    let currentHours: number = this.date.getDate() * 24;
+  getRemainTime (): number {
+    const fullHours: number = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0).getDate() * 24
+    const currentHours: number = this.date.getDate() * 24
 
-    return fullHours - currentHours;
+    return fullHours - currentHours
   }
 }
